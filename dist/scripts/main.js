2a10516960917f08b4682a2a5d51bf60b065a022
Chikoom <callforcopy@gmail.com>
2020-07-26 11:52:10 +0300

    Basic connection in dist folder

---
+++ dist/scripts/main.js
@@ -0,0 +1,8 @@
+import { AppService } from '../model/APPService.js'
+import { User } from '../model/User.js'
+import { Course } from '../model/Course.js'
+import { CourseList } from '../model/CourseList.js'
+import { App } from '../model/App.js'
+import { Render } from '../view/Render.js'
+
+console.log('main')
