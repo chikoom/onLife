c7004778b3b774e1f72a5a5490ecdd4ce33f04a2
Chen Ozer <chen572@gmail.com>
2020-07-26 19:42:54 +0300

    initial crawler

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose.connect("mongodb+srv://onlife_admin:0NL!feD8Pa55@onlife.lb5bu.mongodb.net/onlife?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 const app = express()
