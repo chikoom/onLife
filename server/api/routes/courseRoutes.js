a88c136b31c509d4ee0bd00611af7482059696ad
taldwek <64541444+taldwek@users.noreply.github.com>
2020-07-26 17:07:21 +0300

    get user courses

--- server/api/routes/courseRoutes.js
+++ server/api/routes/courseRoutes.js
@@ -1,16 +1,19 @@
-// const request = require("request");
-// const express = require("express");
-// const User = require("../../models/User");
-// const Course = require("../../models/Course");
-// const router = express.Router();
+const request = require("request");
+const express = require("express");
+const User = require("../../models/User");
+const Course = require("../../models/Course");
+const router = express.Router();
 
-// router.get("/:userId", async (req, res) => {
-// let 
-// res.send('courses')
-// });
+router.get("/:userId", async (req, res) => {
+    let { userId } = req.params
+    // console.log(userId)
+    let courses = await User.find({'_id': userId}, {'courses': 1}).populate('courses.course')
+    res.send(courses)
+}); 
 
-// module.exports = router
+module.exports = router
 
+// GET /api/user/courses/:userId
 // req.body { sort: relevnce / completedFirst, uncompletedFirst }
 // gets the populated list of courses for a user
 // returns a sorted array of courses {course, progress}
