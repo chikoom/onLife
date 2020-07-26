c89aeb30ccc2b25f5aca9dfde0758ae3c399d836
taldwek <64541444+taldwek@users.noreply.github.com>
2020-07-26 13:18:06 +0300

    schemas

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,3 +1,21 @@
-router.get('route', (req, res) => {
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
+const CourseSchema = new Schema({
+    providerCourseId: String,
+    name: String,
+    slug: String,
+    description: String,
+    price: Number,
+    currency: String,
+    provider: {type: Schema.Types.ObjectId, ref: 'Provider'},
+    language: String,
+    subtitlesLanguages: [String],
+    lessons: [{name: String, description: String}],
+    imageURL: String,
+    videoURL: String,
+    courseURL: String
 })
+
+const Course = mongoose.model('Course', CourseSchema)
+module.exports = Course
