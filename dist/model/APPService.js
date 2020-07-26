209c578c792979de176917672e3797dbe8b01ff5
Chikoom <callforcopy@gmail.com>
2020-07-26 20:35:06 +0300

    Styled the courses lists

--- dist/model/APPService.js
+++ dist/model/APPService.js
@@ -1,4 +1,12 @@
 export class AppService {
-
+  async fetchSearchResultsFromDB(searchQuery){
+    return await $.get(`/api/courses?q=${searchQuery}`)
+  }
+  async fetchUserInfoFromDB(userId){
+    return await $.get(`/api/user/${userId}`)
+  }
+  async fetchUserCoursesFromDB(userId){
+    return await $.get(`/api/user/courses/${userId}`)
+  }
 }
 console.log('AppService loaded')
