8865aa0a919e31fccd55d0e46dd35b34eba2eaa2
Chikoom <callforcopy@gmail.com>
2020-07-28 04:53:21 +0300

    handlebars ready

--- chromeExtension/dist/scripts/content_all.js
+++ chromeExtension/dist/scripts/content_all.js
@@ -1,2 +1,8 @@
 console.log('User Browser - Content Script ALL')
+// chrome.storage.sync.get(['foo', 'bar'], function(items) {
+//   message('Settings retrieved', items);
+// });
 
+if (typeof newText === 'string') {
+  console.log(newText)
+}
