7b907aeaca23d436a51d9f6880e721b4a76224c4
Chen Ozer <chen572@gmail.com>
2020-07-29 11:57:52 +0300

    chrom extension login test

--- chromeExtension/dist/scripts/popups.js
+++ chromeExtension/dist/scripts/popups.js
@@ -1,16 +1,57 @@
-// chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
-//   console.log('Settings saved');
-// });
+let loggedUser = { loggedUser: { userName: '', userID: '' } }
 
-// chrome.storage.sync.get(['foo', 'bar'], function(items) {
-//   message('Settings retrieved', items);
-// });
+$('.button-tab').click((event) => {
+    const content = $(event.currentTarget).val()
+    $('#loginSignup-button').text(content)
+    $('#loginSignup-button').val(content.toLowerCase())
+})
 
-// var updateTextTo = 'hello';
-// chrome.tabs.executeScript({
-//     code: `let newText = "${updateTextTo}";`
-// }, function () {
-//     chrome.tabs.executeScript({
-//         file: "content_all.js"
-//     });
-// });
+$('#loginSignup-button').click(async () => {
+    const userName = $('#enter-username').val()
+    const password = $('#enter-pass').val()
+
+    const endPoint = $('#loginSignup-button').val()
+    $.ajax({
+        url: `https://onlife.herokuapp.com/auth/${endPoint}`,
+        method: 'POST',
+        data: { userName, password },
+        success: handleUserLoginSignupSuccess,
+        error: handleUserLoginSignupError
+    })
+})
+
+const checkIfUserLoggedIn = function () {
+    if (localStorage.onLife) {
+        loggedUser = JSON.parse(localStorage.onLife)
+    }
+}()
+
+const renderLoginError = (msg) => {
+    $('#err-msg').text(msg)
+}
+
+const renderSuccessLogin = (username) => {
+    $('#err-msg').text('')
+    $('.loginSignup-container').empty()
+    $('.button-login-signup').hide()
+    $('.button-username').text(username)
+}
+
+
+const logUserIn = (username, userID) => {
+    loggedUser.loggedUser.userID = userID
+    loggedUser.loggedUser.userName = username
+    localStorage.setItem('onLife', JSON.stringify({ loggedUser: { userName, userID } }))
+}
+
+const handleUserLoginSignupSuccess = (res) => {
+    logUserIn(res.userName, res.userID)
+    renderLoginError(`Hello ${res.userName}! Logged in!`)
+    window.setTimeout(function () {
+        renderSuccessLogin(res.userName)
+    }, 1000)
+}
+
+const handleUserLoginSignupError = (e) => {
+    renderLoginError(e.responseText)
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const Course = require('./models/Course')
 const Provider = require('./models/Provider')
 const User = require('./models/User')
@@ -9,13 +10,14 @@ const userRouter = require("./api/routes/userRoutes");
 const coursesRouter = require("./api/routes/courseRoutes");
 const extensionRouter = require("./api/routes/extensionRoutes");
 const authRouter = require('./api/routes/authRoutes')
-
 const Crawler = require('./crawler/Crawler')
 
 require('dotenv').config()
 mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, '../dist')))
 app.use(express.static(path.join(__dirname, '../node_modules')))
